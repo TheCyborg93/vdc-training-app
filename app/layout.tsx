795833959d735de90b8d389aa56eae19da028463
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-24 15:11:44 +0200

    Fix root layout markup

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="de">">
+    <html lang="de">
       <body>
         <AppShell>{children}</AppShell>
       </body>
