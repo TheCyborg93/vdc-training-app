de788896f438459b89f353de9db1c6932f531c06
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-26 21:40:09 +0200

    Add home training history below activity

--- app/heimtraining/layout.tsx
+++ app/heimtraining/layout.tsx
@@ -1,11 +1,14 @@
 import HomeActivityPanel from "@/components/home-training/HomeActivityPanel";
+import HomeHistoryPanel from "@/components/home-training/HomeHistoryPanel";
 import "../vdc-phase5-home-activity.css";
+import "../vdc-phase5-home-history.css";
 
 export default function HomeTrainingLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       {children}
       <HomeActivityPanel />
+      <HomeHistoryPanel />
     </>
   );
 }
