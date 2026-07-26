1099b05386ce5962dd2e8022c77f866ed5d464f7
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-26 15:18:48 +0200

    feat: load phase 1.2 component styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import "./competition-premium.css";
 import "./vdc-sprint4.css";
 import "./vdc-production.css";
 import "./vdc-design-system.css";
+import "./vdc-components.css";
 
 export const metadata: Metadata = {
   title: "VDC Training OS",
