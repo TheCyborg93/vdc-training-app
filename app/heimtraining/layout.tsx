43f299197acf49fc7195c683bcaa9ce91d2b3276
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 06:02:10 +0200

    Phase 5.11 integrate plan library

--- app/heimtraining/layout.tsx
+++ app/heimtraining/layout.tsx
@@ -1,5 +1,6 @@
 import HomeQuickstartPanel from "@/components/home-training/HomeQuickstartPanel";
 import HomeGoalsPanel from "@/components/home-training/HomeGoalsPanel";
+import HomePlanLibrary from "@/components/home-training/HomePlanLibrary";
 import HomeActivityPanel from "@/components/home-training/HomeActivityPanel";
 import HomeHistoryPanel from "@/components/home-training/HomeHistoryPanel";
 import HomeMilestonesPanel from "@/components/home-training/HomeMilestonesPanel";
@@ -7,6 +8,7 @@ import HomeTrendsPanel from "@/components/home-training/HomeTrendsPanel";
 import { HomeInsightsProvider } from "@/components/home-training/HomeInsightsProvider";
 import "../vdc-phase5-home-quickstart.css";
 import "../vdc-phase5-home-goals.css";
+import "../vdc-phase5-home-library.css";
 import "../vdc-phase5-home-activity.css";
 import "../vdc-phase5-home-history.css";
 import "../vdc-phase5-home-milestones.css";
@@ -18,6 +20,7 @@ export default function HomeTrainingLayout({ children }: Readonly<{ children: Re
       {children}
       <HomeQuickstartPanel />
       <HomeGoalsPanel />
+      <HomePlanLibrary />
       <HomeActivityPanel />
       <HomeHistoryPanel />
       <HomeMilestonesPanel />
