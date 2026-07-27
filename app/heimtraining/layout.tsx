e28caf5294a902f98574560341d74a3d36155132
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 05:19:42 +0200

    Phase 5.9: wrap home panels in shared insights provider

--- app/heimtraining/layout.tsx
+++ app/heimtraining/layout.tsx
@@ -3,6 +3,7 @@ import HomeActivityPanel from "@/components/home-training/HomeActivityPanel";
 import HomeHistoryPanel from "@/components/home-training/HomeHistoryPanel";
 import HomeMilestonesPanel from "@/components/home-training/HomeMilestonesPanel";
 import HomeTrendsPanel from "@/components/home-training/HomeTrendsPanel";
+import { HomeInsightsProvider } from "@/components/home-training/HomeInsightsProvider";
 import "../vdc-phase5-home-quickstart.css";
 import "../vdc-phase5-home-activity.css";
 import "../vdc-phase5-home-history.css";
@@ -11,13 +12,13 @@ import "../vdc-phase5-home-trends.css";
 
 export default function HomeTrainingLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
+    <HomeInsightsProvider>
       {children}
       <HomeQuickstartPanel />
       <HomeActivityPanel />
       <HomeHistoryPanel />
       <HomeMilestonesPanel />
       <HomeTrendsPanel />
-    </>
+    </HomeInsightsProvider>
   );
 }
