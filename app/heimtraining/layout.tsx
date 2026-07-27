923c3670df7d6333c74cbb7754f1b1bf752386a1
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 05:05:12 +0200

    Phase 5.8: integrate smart quickstart panel

--- app/heimtraining/layout.tsx
+++ app/heimtraining/layout.tsx
@@ -1,7 +1,9 @@
+import HomeQuickstartPanel from "@/components/home-training/HomeQuickstartPanel";
 import HomeActivityPanel from "@/components/home-training/HomeActivityPanel";
 import HomeHistoryPanel from "@/components/home-training/HomeHistoryPanel";
 import HomeMilestonesPanel from "@/components/home-training/HomeMilestonesPanel";
 import HomeTrendsPanel from "@/components/home-training/HomeTrendsPanel";
+import "../vdc-phase5-home-quickstart.css";
 import "../vdc-phase5-home-activity.css";
 import "../vdc-phase5-home-history.css";
 import "../vdc-phase5-home-milestones.css";
@@ -11,6 +13,7 @@ export default function HomeTrainingLayout({ children }: Readonly<{ children: Re
   return (
     <>
       {children}
+      <HomeQuickstartPanel />
       <HomeActivityPanel />
       <HomeHistoryPanel />
       <HomeMilestonesPanel />
