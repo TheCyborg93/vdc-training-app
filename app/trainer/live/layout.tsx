496493ec6f7a61d558d5bb4c051115238db780e7
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 12:12:18 +0200

    feat: integrate intelligent grouping into live center

--- app/trainer/live/layout.tsx
+++ app/trainer/live/layout.tsx
@@ -1,12 +1,15 @@
 import LiveAttendanceDock from "@/components/trainer/LiveAttendanceDock";
+import LiveGroupingDock from "@/components/trainer/LiveGroupingDock";
 import "../../vdc-phase6-live.css";
 import "../../vdc-phase6-board-management.css";
 import "../../vdc-phase6-attendance.css";
+import "../../vdc-phase6-grouping.css";
 
 export default function TrainerLiveLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       {children}
+      <LiveGroupingDock />
       <LiveAttendanceDock />
     </>
   );
