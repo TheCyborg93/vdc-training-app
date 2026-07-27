f6703bc46076e9ceec6291280225f598566ea10a
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 10:38:38 +0200

    Phase 6.5: mount attendance dock in live layout

--- app/trainer/live/layout.tsx
+++ app/trainer/live/layout.tsx
@@ -1,6 +1,13 @@
+import LiveAttendanceDock from "@/components/trainer/LiveAttendanceDock";
 import "../../vdc-phase6-live.css";
 import "../../vdc-phase6-board-management.css";
+import "../../vdc-phase6-attendance.css";
 
 export default function TrainerLiveLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return children;
+  return (
+    <>
+      {children}
+      <LiveAttendanceDock />
+    </>
+  );
 }
