a86da994d4cbdec7c0288614148878f1f768726b
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-27 04:18:22 +0200

    Add milestones to home training hub

--- app/heimtraining/layout.tsx
+++ app/heimtraining/layout.tsx
@@ -1,7 +1,9 @@
 import HomeActivityPanel from "@/components/home-training/HomeActivityPanel";
 import HomeHistoryPanel from "@/components/home-training/HomeHistoryPanel";
+import HomeMilestonesPanel from "@/components/home-training/HomeMilestonesPanel";
 import "../vdc-phase5-home-activity.css";
 import "../vdc-phase5-home-history.css";
+import "../vdc-phase5-home-milestones.css";
 
 export default function HomeTrainingLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
@@ -9,6 +11,7 @@ export default function HomeTrainingLayout({ children }: Readonly<{ children: Re
       {children}
       <HomeActivityPanel />
       <HomeHistoryPanel />
+      <HomeMilestonesPanel />
     </>
   );
 }
