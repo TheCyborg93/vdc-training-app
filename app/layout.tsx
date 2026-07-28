f5439d2575787057cb2a1c32c0baa1bf5dd35d67
TheCyborg93 <kf95m5v582@privaterelay.appleid.com>
2026-07-28 08:13:30 +0200

    style(ui): load notification center styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ import "./vdc-engine-embed-fix.css";
 import "./vdc-phase5-player.css";
 import "./vdc-phase5-home.css";
 import "./vdc-phase5-activity.css";
+import "./vdc-notifications.css";
 
 export const metadata: Metadata = {
   title: "VDC Training OS",
